7e734919874fcc3ad206473d48241ecae19cee88
Pasztor Adam <adam.psztr@icloud.com>
2022-09-25 21:50:07 +0200

    new index headline
    new index headline with color effect, added more default color

--- scripts/index.js
+++ scripts/index.js
@@ -5,4 +5,19 @@ selectAnchorSetHref('#menuPracticeBtn', './pages/practice.html')
 
 selectElement("#menuSetBtn").addEventListener('click', () => {
 	localStorage.clear();
-})
+});
+
+
+
+function colorRotate() {
+	for (let i=0; i<=selectElement("h1").innerText.length+1; i++) {
+		setTimeout(function(){
+			document.querySelectorAll("main h1 span")[i==0||i==1?19:i-2].style.color = "var(--main-color-light)";
+			document.querySelectorAll("main h1 span")[i==20||i==21?"":i].style.color = "var(--main-color-border)";
+		}, 200 + i * 200);
+	}
+};
+
+colorRotate();
+setInterval(colorRotate, 4400);
+
